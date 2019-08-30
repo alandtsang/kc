5f2c7e540209a9ae4f03493bdc47b2adea8edddd
Aland <zengxianglong0@gmail.com>
2019-08-30 13:13:26 +0800

    resources: support get specified ServiceAccounts name

--- resources/sa.go
+++ resources/sa.go
@@ -2,7 +2,9 @@ package resources
 
 import (
 	"fmt"
+	"log"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -18,15 +20,39 @@ func NewServiceAccounts(clientSet *kubernetes.Clientset, namespace, name string)
 }
 
 func (sa *ServiceAccounts) Get() {
+	if len(sa.name) > 0 {
+		sa.GetServiceAccount()
+	} else {
+		sa.GetServiceAccountList()
+	}
+}
+
+func (sa *ServiceAccounts) GetServiceAccount() {
+	serviceAccount, err := sa.clientSet.CoreV1().ServiceAccounts(sa.namespace).Get(sa.name, metav1.GetOptions{})
+	if err != nil {
+		log.Fatalln("[Error] GetServiceAccount()", err.Error())
+	}
+	sa.printServiceAccount(serviceAccount)
+}
+
+func (sa *ServiceAccounts) GetServiceAccountList() {
 	serviceAccounts, err := sa.clientSet.CoreV1().ServiceAccounts(sa.namespace).List(metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		log.Fatalln("[Error] GetServiceAccountList()", err.Error())
 	}
+	sa.printServiceAccountList(serviceAccounts)
+}
 
+func (sa *ServiceAccounts) printServiceAccount(s *v1.ServiceAccount) {
+	fmt.Printf("NAME\t\t\t SECRETS\t AGE\n")
+	fmt.Printf("%-24s %d\t %s\n", s.Name, len(s.Secrets), s.CreationTimestamp)
+}
+
+func (sa *ServiceAccounts) printServiceAccountList(serviceAccounts *v1.ServiceAccountList) {
 	if len(serviceAccounts.Items) > 0 {
 		fmt.Printf("NAME\t\t\t SECRETS\t AGE\n")
-		for _, sa := range serviceAccounts.Items {
-			fmt.Printf("%-24s %d\t %s\n", sa.Name, len(sa.Secrets), sa.CreationTimestamp)
+		for _, s := range serviceAccounts.Items {
+			fmt.Printf("%-24s %d\t %s\n", s.Name, len(s.Secrets), s.CreationTimestamp)
 		}
 	} else {
 		fmt.Println(ERR_NO_RESOURCE)
