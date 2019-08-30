48ad3c017106dd156578c64475f4c89043042f7f
Aland <zengxianglong0@gmail.com>
2019-08-30 13:36:22 +0800

    resources: support get specified Services name

--- resources/service.go
+++ resources/service.go
@@ -2,7 +2,9 @@ package resources
 
 import (
 	"fmt"
+	"log"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -18,11 +20,37 @@ func NewServices(clientSet *kubernetes.Clientset, namespace, name string) *Servi
 }
 
 func (s *Services) Get() {
+	if len(s.name) > 0 {
+		s.getService()
+	} else {
+		s.getServiceList()
+	}
+}
+
+func (s *Services) getService() {
+	service, err := s.clientSet.CoreV1().Services(s.namespace).Get(s.name, metav1.GetOptions{})
+	if err != nil {
+		log.Fatalln("[Error] getService()", err.Error())
+	}
+	s.printService(service)
+}
+
+func (s *Services) getServiceList() {
 	services, err := s.clientSet.CoreV1().Services(s.namespace).List(metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		log.Fatalln("[Error] getServiceList()", err.Error())
 	}
+	s.printServiceList(services)
+}
 
+func (s *Services) printService(service *v1.Service) {
+	fmt.Printf("NAME\t\t TYPE\t\t CLUSTER-IP\t EXTERNAL-IP\t PORT(S)\t AGE\n")
+	fmt.Printf("%-16s %s\t %s\t %v\t\t %v\t %s\n",
+		service.Name, service.Spec.Type, service.Spec.ClusterIP,
+		service.Spec.ExternalIPs, service.Spec.Ports, service.CreationTimestamp)
+}
+
+func (s *Services) printServiceList(services *v1.ServiceList) {
 	if len(services.Items) > 0 {
 		fmt.Printf("NAME\t\t TYPE\t\t CLUSTER-IP\t EXTERNAL-IP\t PORT(S)\t AGE\n")
 		for _, service := range services.Items {
