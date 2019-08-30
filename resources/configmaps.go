059893e3c4f6bed1a4801aa27e7a0556164b5086
Aland <zengxianglong0@gmail.com>
2019-08-30 10:39:08 +0800

    resources: support get specified ConfigMaps name

--- resources/configmaps.go
+++ resources/configmaps.go
@@ -2,7 +2,9 @@ package resources
 
 import (
 	"fmt"
+	"log"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -18,11 +20,35 @@ func NewConfigMaps(clientSet *kubernetes.Clientset, namespace, name string) *Con
 }
 
 func (cm *ConfigMaps) Get() {
+	if len(cm.name) > 0 {
+		cm.GetConfigMap()
+	} else {
+		cm.GetConfigMaps()
+	}
+}
+
+func (cm *ConfigMaps) GetConfigMap() {
+	configMap, err := cm.clientSet.CoreV1().ConfigMaps(cm.namespace).Get(cm.name, metav1.GetOptions{})
+	if err != nil {
+		log.Fatalln("[Error] GetConfigMap()", err.Error())
+	}
+	cm.printConfigMap(configMap)
+}
+
+func (cm *ConfigMaps) GetConfigMaps() {
 	configMaps, err := cm.clientSet.CoreV1().ConfigMaps(cm.namespace).List(metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		log.Fatalln("[Error] GetConfigMaps()", err.Error())
 	}
+	cm.printConfigMaps(configMaps)
+}
 
+func (cm *ConfigMaps) printConfigMap(configMap *v1.ConfigMap) {
+	fmt.Printf("NAME\t\t\t DATA\t AGE\n")
+	fmt.Printf("%-24s %d\t %s\n", configMap.Name, len(configMap.Data), configMap.CreationTimestamp)
+}
+
+func (cm *ConfigMaps) printConfigMaps(configMaps *v1.ConfigMapList) {
 	if len(configMaps.Items) > 0 {
 		fmt.Printf("NAME\t\t\t DATA\t AGE\n")
 		for _, configMap := range configMaps.Items {
